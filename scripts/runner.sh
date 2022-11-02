ee6662d2e5c949d3012ece82808ea8e333d0b21a
Romain Lespinasse <romain.lespinasse@gmail.com>
2022-11-02 20:51:04 +0100

    feat: remove warning filtering

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "${ELECTRON_DISABLE_SECURITY_WARNINGS}" == "true" ]; then
-  "$DRAWIO_DESKTOP_EXECUTABLE_PATH" "$@" --no-sandbox 3>&1 >&2 2>&3 3>&- |
-    grep -v "Failed to connect to socket" |
-    grep -v "Could not parse server address" |
-    grep -v "Floss manager not present" |
-    grep -v "Exiting GPU process" |
-    grep -v "called with multiple threads" |
-    grep -v "extension not supported" |
-    grep -v "Failed to send GpuControl.CreateCommandBuffer"
-else
-  "$DRAWIO_DESKTOP_EXECUTABLE_PATH" "$@" --no-sandbox 2>&1
-fi
+"$DRAWIO_DESKTOP_EXECUTABLE_PATH" "$@" --no-sandbox
