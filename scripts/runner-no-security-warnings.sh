b6255fa2e96619bde2218b2c08bc3f63bede3e38
Romain Lespinasse <romain.lespinasse@gmail.com>
2022-11-02 21:16:23 +0100

    fix: use previous election security warning filtering

--- scripts/runner-no-security-warnings.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-"$DRAWIO_DESKTOP_EXECUTABLE_PATH" "$@" --no-sandbox 2>&1
--- scripts/runner.sh
+++ scripts/runner.sh
@@ -2,14 +2,7 @@
 set -e
 
 if [ "${ELECTRON_DISABLE_SECURITY_WARNINGS}" == "true" ]; then
-  "$DRAWIO_DESKTOP_EXECUTABLE_PATH" "$@" --no-sandbox 3>&1 >&2 2>&3 3>&- |
-    grep -v "Failed to connect to socket" |
-    grep -v "Could not parse server address" |
-    grep -v "Floss manager not present" |
-    grep -v "Exiting GPU process" |
-    grep -v "called with multiple threads" |
-    grep -v "extension not supported" |
-    grep -v "Failed to send GpuControl.CreateCommandBuffer"
+  "$DRAWIO_DESKTOP_EXECUTABLE_PATH" "$@" --no-sandbox 2> >(grep -v "Failed to connect to socket\|Could not parse server address\|Floss manager not present\|Exiting GPU process\|called with multiple threads\|extension not supported\|Failed to send GpuControl.CreateCommandBuffer")
 else
   "$DRAWIO_DESKTOP_EXECUTABLE_PATH" "$@" --no-sandbox 2>&1
 fi
