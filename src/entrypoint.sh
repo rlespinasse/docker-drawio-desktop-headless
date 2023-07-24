63d6dac47db6ffe06e31411276ac879e7b04c270
Romain Lespinasse <romain.lespinasse@gmail.com>
2023-07-24 14:08:00 +0200

    remove autoupdate logs

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,6 +7,12 @@ if [[ "${ELECTRON_DISABLE_SECURITY_WARNINGS:?}" == "true" ]]; then
   cat "${DRAWIO_DESKTOP_SOURCE_FOLDER:?}/unwanted-security-warnings.txt" >>"${DRAWIO_DESKTOP_SOURCE_FOLDER:?}/unwanted-lines.txt"
 fi
 
+if [[ "${DRAWIO_DISABLE_UPDATE:?}" == "true" ]]; then
+  # Remove 'deb support' logs
+  # since 'autoUpdater.logger.transports.file.level' is set as 'info' on drawio-desktop
+  cat "${DRAWIO_DESKTOP_SOURCE_FOLDER:?}/unwanted-security-warnings.txt" >>"${DRAWIO_DESKTOP_SOURCE_FOLDER:?}/unwanted-lines.txt"
+fi
+
 # Start Xvfb
 export DISPLAY="${XVFB_DISPLAY:?}"
 # shellcheck disable=SC2086
