27f2f923b42004e4dfd0104323c7363de5619776
Romain Lespinasse <romain.lespinasse@gmail.com>
2023-07-24 14:26:47 +0200

    fix unwanted update logs filename

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -10,7 +10,7 @@ fi
 if [[ "${DRAWIO_DISABLE_UPDATE:?}" == "true" ]]; then
   # Remove 'deb support' logs
   # since 'autoUpdater.logger.transports.file.level' is set as 'info' on drawio-desktop
-  cat "${DRAWIO_DESKTOP_SOURCE_FOLDER:?}/unwanted-security-warnings.txt" >>"${DRAWIO_DESKTOP_SOURCE_FOLDER:?}/unwanted-lines.txt"
+  cat "${DRAWIO_DESKTOP_SOURCE_FOLDER:?}/unwanted-update-logs.txt" >>"${DRAWIO_DESKTOP_SOURCE_FOLDER:?}/unwanted-lines.txt"
 fi
 
 # Start Xvfb
