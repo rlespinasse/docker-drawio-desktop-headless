1b83d00c0215eaa09580c810fca4d076ede1a69c
Romain Lespinasse <romain.lespinasse@gmail.com>
2026-07-14 00:28:39 +0200

    fix: silence Electron D-Bus and GPU-probe warnings at the source (#146)

--- src/runner.sh
+++ src/runner.sh
@@ -5,4 +5,6 @@ if [[ "${SCRIPT_DEBUG_MODE:-false}" == "true" ]]; then
   set -x
 fi
 
-"${DRAWIO_DESKTOP_EXECUTABLE_PATH:?}" "$@" --no-sandbox --disable-gpu
+"${DRAWIO_DESKTOP_EXECUTABLE_PATH:?}" "$@" --no-sandbox --disable-gpu \
+  --disable-features=VaapiVideoDecoder,VaapiVideoEncoder \
+  --disable-accelerated-video-decode --disable-accelerated-video-encode
